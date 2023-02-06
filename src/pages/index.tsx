ea81660383952fdd8efd7bb336a934b30ed721a2
David-Leatham <david@leatham.com>
2023-02-06 21:47:01 +0100

    Fort commit. UI is fucked up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,84 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import React, { useState, useEffect, useRef } from "react";
 
 const inter = Inter({ subsets: ['latin'] })
 
+
+function getrDivList (arr: Array<number>) {
+  let divList: Array<JSX.Element> = [];
+  for (let i = 0; i < arr[1]; i++) {
+    for (let j = 0; j < arr[0]; j++) {
+      divList.push(<div className={styles.checkbox} key={j + i * arr[0]}/>);
+    }
+  }
+  return divList;
+}
+
+function getBoxCount(width: number, height: number) {
+  let boxSize = 25;
+  let distance = 5;
+  let horizontalCount = Math.floor((width) / (boxSize + distance))
+  // console.log(width - (boxSize + distance) * horizontalCount + distance)
+  if ( width - (boxSize + distance) * horizontalCount + distance >= boxSize) {
+    horizontalCount += 1
+  }
+  let verticalCount = Math.floor((height) / (boxSize + distance))
+  // console.log(height, (boxSize + distance) * verticalCount + distance)
+  // if ( height - (boxSize + distance) * verticalCount >= boxSize) {
+  //   verticalCount += 1
+  // }
+  return [horizontalCount, verticalCount]
+}
+
+function Middle() {
+  const [boardSize, setBoardSize] = useState([0, 0]);
+  const middleDivRef = useRef(null);
+
+  if (typeof window !== 'undefined') {
+    const [resizeBoard, setResizeBoard] = useState(document.getElementsByClassName(styles.middle)[0] as HTMLElement);
+
+    // create element ref
+
+    useEffect(() => {
+      const resizeObserver = new ResizeObserver((entries) => {
+
+        for (const entry of entries) {
+          if (entry.contentBoxSize) {
+            console.log(entry.contentRect.width, (document.getElementsByClassName(styles.middle)[0] as HTMLElement).clientWidth)
+            setBoardSize(getBoxCount(Math.round(entry.contentRect.width), entry.contentRect.height));
+            // console.log('repeat(' + boardSize[0] + ', 10fr)')
+            // resizeBoard.style.gridTemplateColumns = 'repeat(' + boardSize[0] + ', 0fr)'
+            console.log(boardSize)
+            resizeBoard.style.gridTemplateRows = 'repeat(' + boardSize[1] + ', 2fr)'
+            // resizeBoard.style.backgroundColor = 'black';
+          }
+        }
+      });
+
+      if (middleDivRef.current) {
+        resizeObserver.observe(middleDivRef.current);
+      }
+      return () => {
+        if (middleDivRef.current) {
+          resizeObserver.observe(middleDivRef.current);
+        }
+      };
+    }, []);
+  }
+
+  return (
+    <div className={styles.middle} ref={middleDivRef}>
+      { getrDivList(boardSize) }
+    </div>
+  )
+
+}
+
+
 export default function Home() {
+
   return (
     <>
       <Head>
@@ -15,108 +89,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
+        <div className={styles.startButtonOuter}>
+          <button className={styles.startButtonInner + ' ' + inter.className}>Start</button>
         </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        <Middle></Middle>
       </main>
     </>
   )
