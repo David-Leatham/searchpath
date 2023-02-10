74746038352f157a422da000e438c1d2686bebfc
David-Leatham <david@leatham.de>
2023-02-10 23:52:42 +0100

    Added width first dijaks algorithm

--- src/lib/searchalgorithms/algorithms/dijkstraWidthFirst.ts
+++ src/lib/searchalgorithms/algorithms/dijkstraWidthFirst.ts
@@ -4,32 +4,55 @@ import { getValidNeighbours } from '../helpers'
 
 
 export default function dijkstraWidthFirst(start: number, finish: number, board: Board): SearchPath {
-  // let positions = getValidNeighbours(start, board);
-  let searchPath: SearchPath =  {searchList: [], shortestPath: []};
-  let positions = dijkstraWidthFirstHelper(getValidNeighbours(start, board), searchPath, board);
-  return searchPath
-}
+  let searchList: Array<number> = [];
+  let shortestPath: Array<number> = [];
 
-function dijkstraWidthFirstHelper(positionToSearch: Array<number>, searchPath: SearchPath, board: Board): boolean {
-  let validPositions: Array<number> = []
-  for (let position of positionToSearch) {
-    if (!searchPath.searchList.includes(position)) {
-      if (board.boardList[position] == Block.Finish) {
-        searchPath.searchList.push(position);
-        searchPath.shortestPath.push(position);
-        return true
-      } else {
-        searchPath.searchList.push(position);
-        validPositions.push(position)
+  let neighbors: Array<number> = getValidNeighbours(start, board);
+  let moves: Array<Array<number>> = [];
+  let finished: boolean = false;
+  searchList.push(start);
+  let pastList: Array<Array<number>> = [...neighbors].map(neighbor => [start, neighbor]);
+  pastList.push([start, start]);
+
+  while (neighbors.length > 0) {
+    let nextBlock = neighbors.shift()
+    if (nextBlock) { // Just for ts..
+      for (let move of pastList) {
+        if (move[1] == nextBlock) {
+          moves.push([move[0], nextBlock])
+        }
+      }
+      // finish.
+      // moves.push([nextBlock, newBlock]);
+      // nextBlock = newBlock;
+      searchList.push(nextBlock);
+      if (board.boardList[nextBlock] == Block.Finish) {
+        finished = true;
+        break
+      }
+      for (let neighbor of getValidNeighbours(nextBlock, board)) {
+        	if (!(neighbors.includes(neighbor)) && !(searchList.includes(neighbor))) {
+            pastList.push([nextBlock, neighbor])
+            neighbors.push(neighbor)
+          }
+      }
+    } else {
+      break
+    }
+  }
+  if (finished) {
+    let currentBlock = finish;
+    shortestPath.push(currentBlock);
+    while (currentBlock != start) {
+      for (let move of moves) {
+        if (move[1] == currentBlock) {
+          currentBlock = move[0]
+          shortestPath.push(currentBlock)
+          break
+        }
       }
     }
   }
-  for (let validPosition of validPositions) {
-    // searchPath.searchList.push(validPosition);
-    if (dijkstraWidthFirstHelper(getValidNeighbours(validPosition, board), searchPath, board)) {
-      searchPath.shortestPath.push(validPosition)
-      return true
-    }
-  }
-  return false
+  let searchPath: SearchPath =  {searchList: searchList, shortestPath: shortestPath};
+  return searchPath
 }
