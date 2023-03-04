17b31f0266e258a7bb221c0d899be46917818ec3
David-Leatham <david@leatham.de>
2023-03-04 15:14:05 +0100

    Renaming to naming convention

--- src/lib/mazes/algorithms/recursiveDivision.ts
+++ src/lib/mazes/algorithms/recursiveDivision.ts
@@ -5,14 +5,14 @@ export default function empty(height: number, width: number): Array<Block> {
   let grid = getInnerPathArray(height, width);
   if (!grid) {return []}
 
-  recursive_divide(grid, 0, height-2, 0, width-2)
+  recursiveDivide(grid, 0, height-2, 0, width-2)
 
   innerPathArrayAddStartFinish(grid);
   return innerPathArrayToOut(width, height, grid);
 }
 
 
-function recursive_divide(grid: Array<Array<Block>>, heightLowerBound: number, heightUpperBound: number, widthLowerBound: number, widthUpperBound: number) {
+function recursiveDivide(grid: Array<Array<Block>>, heightLowerBound: number, heightUpperBound: number, widthLowerBound: number, widthUpperBound: number) {
   if (!(heightUpperBound - heightLowerBound >= 2 && widthUpperBound - widthLowerBound >= 2 && heightUpperBound - heightLowerBound + widthUpperBound - widthLowerBound >= 5)) {
     return
   }
@@ -34,8 +34,8 @@ function recursive_divide(grid: Array<Array<Block>>, heightLowerBound: number, h
         grid[borderHeight][i] = Block.Wall;
       }
     }
-    recursive_divide(grid, heightLowerBound, borderHeight, widthLowerBound, widthUpperBound);
-    recursive_divide(grid, borderHeight+1, heightUpperBound, widthLowerBound, widthUpperBound);
+    recursiveDivide(grid, heightLowerBound, borderHeight, widthLowerBound, widthUpperBound);
+    recursiveDivide(grid, borderHeight+1, heightUpperBound, widthLowerBound, widthUpperBound);
   } else {
     let borderWidth = getRandomInt(widthLowerBound+1, widthUpperBound-1, false, true)
     let borderHole = getRandomInt(heightLowerBound, heightUpperBound, true, false)
@@ -44,7 +44,7 @@ function recursive_divide(grid: Array<Array<Block>>, heightLowerBound: number, h
         grid[i][borderWidth] = Block.Wall;
       }
     }
-    recursive_divide(grid, heightLowerBound, heightUpperBound, widthLowerBound, borderWidth);
-    recursive_divide(grid, heightLowerBound, heightUpperBound, borderWidth+1, widthUpperBound);
+    recursiveDivide(grid, heightLowerBound, heightUpperBound, widthLowerBound, borderWidth);
+    recursiveDivide(grid, heightLowerBound, heightUpperBound, borderWidth+1, widthUpperBound);
   }
 }
